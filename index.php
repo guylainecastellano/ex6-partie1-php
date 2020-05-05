736bc8d0a8b75ae02057f703cde21daca331d3cc
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-05-05 17:43:53 +0200

    ex6 partie1 php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$number = '140';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex1 partie1 php</title>
+</head>
+<body>
+  <?php
+  echo ($number + 30) / 2 ;
+  ?>
+</body>
+</html>
