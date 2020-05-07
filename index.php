4828496c5f35c073e04efb99f562a6acf7f248da
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-07 09:33:40 +0200

    ex6 partie1 php

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
-$number = '140';
+$number = 140; //1ere version
+$number = ($number + 30) / 2
+//3e version $number = 140;
+//$number += 30;
+//$number /=2;
+//4e version $number = 140;
+//$number = $number + 30;
+//$number = $number / 2;
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8"/>
-  <title>ex1 partie1 php</title>
+  <title>ex6 partie1 php</title>
 </head>
 <body>
+  <p>Résultat :
   <?php
-  echo ($number + 30) / 2 ;
+  echo $number;
+  //1er version soit echo ($number + 30) / 2
   ?>
+</p>
 </body>
 </html>
